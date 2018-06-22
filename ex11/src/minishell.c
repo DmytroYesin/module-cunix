647701305eba58ec5d6a914c32838084869a7996
dmytro <dmytroyesin@gmail.com>
2018-06-22 15:57:09 +0300

    ex11

---
+++ ex11/src/minishell.c
@@ -0,0 +1,248 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <../include/hash.h>
+#include <../include/linked_list.h>
+
+int fcd(hashtable_t *env, char **args);
+int fexit(hashtable_t *env, char **args);
+int fenv(hashtable_t *env, char **args);
+int fexport(hashtable_t *env, char **args);
+int fecho(hashtable_t *env, char **args);
+
+char *builtin_str[] = {
+  "cd",
+  "exit",
+  "env",
+  "export",
+  "echo"
+};
+
+int fecho(hashtable_t *env, char **args)
+{
+  int n = 1;
+  while(args[n] != NULL)
+  {
+    if(args[n][0] == '$')
+    {
+      write(1, hash_get(env, args[n]+1), strlen(args[n])+1);
+      n++;
+      continue;
+    }
+    write(1, args[n], strlen(args[n])+1);
+    n++;
+  }
+  write(1, "\n", 1);
+  return 1;
+}
+
+int fexport(hashtable_t *env, char **args)
+{
+  char *var, *var_point, *value_point;
+  value_point = strtok(args[1], "=");
+  var = malloc((strlen(value_point)+1)* sizeof(char));
+  strcpy(var, value_point);
+  while (value_point != NULL)
+  {
+    var_point = malloc((strlen(value_point)+1)*sizeof(char));
+    strcpy(var_point, value_point);
+    value_point = strtok(NULL, "=");
+  }
+  if (!strcmp(var, var_point))
+    return 1;
+
+      hash_set(env, var, var_point);
+  write(1,"\n",1);
+  return 1;
+}
+
+int fenv(hashtable_t *env, char **args)
+{
+  hash_print(env);
+  write(1,"\n",1);
+  return 1;
+}
+
+
+int num_b_ins()
+{
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int fcd(hashtable_t *env, char **args)
+{
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "expected argument to \"cd\"\n");
+  }
+  else
+  {
+    if (chdir(args[1]) != 0)
+    {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int fexit(hashtable_t *env, char **args)
+{
+  return 0;
+}
+
+int m_launch(hashtable_t *env, char **args)
+{
+  pid_t pid;
+  int status;
+  pid = fork();
+  if (pid == 0)
+  {
+    if (execvp(args[0], args) == -1)
+    {
+      perror("msh");
+    }
+    exit(EXIT_FAILURE);
+  }
+  else if (pid < 0)
+  {
+    perror("msh");
+  }
+  else
+  {
+    do
+    {
+      waitpid(pid, &status, WUNTRACED);
+    }
+    while(!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
+int m_execute(hashtable_t *env, char **args)
+{
+  if (args[0] == NULL)
+  {
+    return 1;
+  }
+    if (strcmp(args[0], builtin_str[0]) == 0)
+    {
+      return fcd(env, args);
+    }
+    else if (strcmp(args[0], builtin_str[1]) == 0)
+    {
+      return fexit(env, args);
+    }
+    else if (strcmp(args[0], builtin_str[2]) == 0)
+    {
+      return fenv(env, args);
+    }
+    else if (strcmp(args[0], builtin_str[3]) == 0)
+    {
+      return fexport(env, args);
+    }
+    else if (strcmp(args[0], builtin_str[4]) == 0)
+    {
+      return fexport(env, args);
+    }
+  return m_launch(env, args);
+}
+
+#define LSH_RL_BUFSIZE 1024
+char *m_read(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int sym;
+  if(!buffer)
+  {
+    fprintf(stderr, "allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  while(1)
+  {
+    sym = getchar();
+    if (sym == EOF)
+    {
+      exit(EXIT_SUCCESS);
+    } else if (sym == '\n')
+    {
+      buffer[position] = '\0';
+      return buffer;
+    }
+    else
+    {
+      buffer[position] = sym;
+    }
+    position++;
+    if (position >= bufsize)
+    {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer)
+      {
+        fprintf(stderr, "allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **split_l(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, pos = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens)
+  {
+    fprintf(stderr, "allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL)
+  {
+    tokens[pos] = token;
+    pos++;
+    if (pos >= bufsize)
+    {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens)
+      {
+		  free(tokens_backup);
+      fprintf(stderr, "allocation error\n");
+      exit(EXIT_FAILURE);
+      }
+    }
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[pos] = NULL;
+  return tokens;
+}
+
+int main(int argc, char **argv, char **envv)
+{
+char *line;
+char **args;
+hashtable_t *ht;
+ht = hash_create(128);
+int status;
+do {
+   printf("$_> ");
+   line = m_read();
+   args = split_l(line);
+   status = m_execute(ht, args);
+   free(line);
+   free(args);
+   }
+while (status);
+  return EXIT_SUCCESS;
+}
+
