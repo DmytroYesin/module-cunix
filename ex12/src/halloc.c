704476e096f5e23ea69efb06340bc6af82cf85ef
dmytro <dmytroyesin@gmail.com>
2018-07-05 11:57:30 +0300

    ex12

---
+++ ex12/src/halloc.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+#define STRUCT_SIZE sizeof(struct halloc_block)
+
+void *base = NULL;
+
+typedef struct halloc_block block_h;
+
+struct halloc_block
+{
+  size_t size;
+  int flag;
+  struct halloc_block *n_ptr;
+};
+
+block_h *search(block_h **prev , size_t size)
+{
+  block_h *now = base;
+
+  while(now && !(now -> flag && now -> size >= size))
+  {
+   *prev = now;
+   now = now -> n_ptr;
+  }
+  return now;
+}
+
+block_h *prolong_mem(block_h *prev, size_t size)
+{
+  block_h *new;
+  new = sbrk(0);
+
+  if(sbrk(STRUCT_SIZE + size) == (void*) - 1)
+    return (NULL);
+  new -> size = size;
+  new -> n_ptr = NULL;
+  if(prev)
+  {
+    prev -> n_ptr = new;
+  }
+  new -> flag = 0;
+  return (new);
+}
+
+void *halloc(size_t size)
+{
+  block_h *now;
+  block_h *prev;
+
+  if(size <= 0)
+    return NULL;
+  if(base)
+  {
+    prev = base;
+    now = search(&prev, size);
+    if(now)
+    {
+      now -> flag = 0;
+    }
+    else
+    {
+      now = prolong_mem(prev, size);
+      if (!now)
+        return(NULL);
+    }
+  }
+  else
+  {
+    now = prolong_mem(NULL, size);
+    if (!now)
+      return(NULL);
+    base = now;
+  }
+  return(now + 1);
+}
+
+block_h *block_get(void *addr)
+{
+  block_h *res;
+  res = (block_h *)addr - 1;
+  return res;
+}
+
+void free(void *addr)
+{
+  if(addr)
+  {
+    block_h *targ = block_get(addr);
+    targ -> flag = 1;
+    base = NULL;
+  }
+  else
+    return;
+}
+
